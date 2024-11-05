f02baf07ede21de7cc8f7717aafa6e6973398fb8
Thao225 <buithienhuongthao2005@gmail.com>
2024-11-05 14:54:57 +0700

    up

--- index.js
+++ index.js
@@ -4,116 +4,190 @@ import path from 'path';
 // var path = require('path');
 const app = express();
 const port = 3000;
-await connect('mongodb://localhost/todo-app');
-// mongoose.connection.on("connected",function(){
-//     console.log("Connected to database")
-// })
-// mongoose.connection.on("disconnected",function(){
-//     console.log("Disconnected to database")
-// })
-try {
-    // Kết nối tới cụm MongoDB
-    await client.connect();
-    console.log('Đã kết nối thành công đến cụm MongoDB');
-    // Gọi các hàm truy vấn cơ sở dữ liệu ở đây
-    await listDatabases(client);
-    // Ngắt kết nối với cụm MongoDB
-    await client.close();
-    console.log('Đã ngắt kết nối với cụm MongoDB');
-  } 
-    catch (error) {
-    console.error('Đã xảy ra lỗi:', error);
-  }
+// await connect('mongodb://localhost:27017/todo-app');
+// // mongoose.connection.on("connected",function(){
+// //     console.log("Connected to database")
+// // })
+// // mongoose.connection.on("disconnected",function(){
+// //     console.log("Disconnected to database")
+// // })
+// try {
+//     // Kết nối tới cụm MongoDB
+//     await client.connect();
+//     console.log('Đã kết nối thành công đến cụm MongoDB');
+//     // Gọi các hàm truy vấn cơ sở dữ liệu ở đây
+//     await listDatabases(client);
+//     // Ngắt kết nối với cụm MongoDB
+//     await client.close();
+//     console.log('Đã ngắt kết nối với cụm MongoDB');
+//   } 
+//     catch (error) {
+//     console.error('Đã xảy ra lỗi:', error);
+//   }
 
-const taskSchema = new Schema({
-    temperature:{type:Number},
-    humidity: {type:Number},
-    task: String,
-    isCompleted: Boolean,
-    createdAt: { type: Date, default: Date.now }
-});
+// const taskSchema = new Schema({
+//     task: String,
+//     isCompleted: Boolean,
+//     isDelete: Boolean,
+//     createdAt: { type: Date, default: Date.now }
+// });
 
-const Task = model('Task', taskSchema);
-let tasks = [];
+// // const Task = mongoose.model('Task', taskSchema);
 
-//Kiểm tra hoạt động server
-app.get('/test', function(request, response) {
-    console.log("Received 'test' request");
-    response.status(200).json({"message":"Success"});
-});
-//Lưu trữ dữ liệu lện mongodb
-app.post("/tasks",function(request, response){
-    console.log("Received 'create tasks data' request");
-    //lấy dữ liệu từ query của request:
-    var newTask = new Task({
-        temperature: request.query.temperature,
-        humidity: request.query.humidity
-    });
-    //Lưu
-    newTask.save(function(err){
-        //xuất thông báo ra console và phản hồi về client
-        if (!err) {
-            console.log('Saved new task data');
-            response.status(200).json({'message':'Success'});      
+// //Kiểm tra hoạt động server
+// app.get('/test', function(request, response) {
+//     console.log("Received 'test' request");
+//     response.status(200).json({"message":"Success"});
+// });
+// //Lưu trữ dữ liệu lện mongodb
+// app.post("/tasks",function(request, response){
+//     console.log("Received 'create tasks data' request");
+//     //lấy dữ liệu từ query của request:
+//     var newTask = new Task({
+//         task: request.query.task,
+//         isCompleted: request.query.isCompleted,
+//         isDelete: request.query.isDelete
+//     });
+//     //Lưu
+//     newTask.save(function(err){
+//         //xuất thông báo ra console và phản hồi về client
+//         if (!err) {
+//             console.log('Saved new task data');
+//             response.status(200).json({'message':'Success'});      
+//         }
+//         else {
+//             console.log('No save new task data');
+//             response.status(400).json({'message':'Error'});      
+//         }
+//     });
+// });
+
+// //lấy dữ liệu từ mongodb
+// app.get('/tasks', (req, res) => {
+//     console.log('Get task data');
+//   });
+
+// app.get('/tasks', async (req, res) => {
+//     const tasks = await Task.find();
+//     res.json(tasks);
+// });
+// // Route để lấy danh sách tất cả các tasks
+// app.get('/tasks', (req, res) => {
+//     res.json(tasks);
+//   });
+  
+//   // Route để tạo một task mới
+//   app.post('/tasks', (req, res) => {
+//     const newTask = req.body;
+//     tasks.push(newTask);
+//     res.json(newTask);
+//   });
+  
+//   // Route để lấy thông tin một task cụ thể
+//   app.get('/tasks/:id', (req, res) => {
+//     const id = req.params.id;
+//     const task = tasks.find(task => task.id === id);
+//     if (task) {
+//       res.json(task);
+//     } else {
+//       res.status(404).json({ message: 'Task not found'});
+//     }
+//   });
+  
+//   // Route để cập nhật thông tin một task
+//   app.put('/tasks/:id', (req, res) => {
+//     const id = req.params.id;
+//     const updatedTask = req.body;
+//     const index = tasks.findIndex(task => task.id === id);
+//     if (index !== -1) {
+//       tasks[index] = { ...tasks[index], ...updatedTask };
+//       res.json(tasks[index]);
+//     } else {
+//       res.status(404).json({ message: 'Task not found' });
+//     }
+//   });
+  
+//   // Route để xóa một task
+//   app.delete('/tasks/:id', (req, res) => {
+//     const id = req.params.id;
+//     tasks = tasks.filter(task => task.id !== id);
+//     res.json({ message: 'Task deleted' });
+//   });
+
+mongoose.connect('mongodb://localhost:27017/todo-app')
+var db=mongoose.connection;
+db.on('error', console.log.bind(console, "connection error"));
+db.once('open', function(callback){
+    console.log("connection succeeded");
+})
+
+
+app.get('/', function(req, res, next) {
+    // fetch and sort course collection by id in descending order
+    req.db.collection('course').find().sort({"_id": -1}).toArray(function(err, result) {
+        //if (err) return console.log(err)
+        if (err) {
+            req.flash('error', err)
+            res.render('user/list', {
+                title: 'User List',
+                data: ''
+            })
+        } else {
+            // render to views/user/list.ejs template file
+            res.render('user/list', {
+                title: 'User List',
+                data: result
+            })
         }
-        else {
-            console.log('No save new task data');
-            response.status(400).json({'message':'Error'});      
-        }
+    })
+})
+
+// SHOW ADD course FORM
+app.get('/add', function(req, res, next){
+    // render to views/user/add.ejs
+    res.render('user/add', {
+        title: 'Add course',
+        topic: '',
+        price: '',
+        location: '',
+        provider:'',
+        review_rankings: '',
+        author_ranking: ''
+    })
+})
+
+
+app.post('/add', function(req,res, next){
+    var topic = req.body.topic;
+    var price =req.body.price;
+    var location = req.body.location;
+    var provider = req.body.provider;
+    var review_rankings = req.body.review_rankings;
+    var author_ranking =req.body.author_ranking;
+
+    var data = {
+        "topic": topic,
+        "price":price,
+        "location":location,
+        "provider":provider,
+        "review_rankings":review_rankings,
+        "author_ranking": author_ranking
+    };
+
+    db.collection('course').insertOne(data,function(err, collection){
+
+
+        if (err) throw err;
+        console.log("Record inserted Successfully");
+
     });
-});
 
-//lấy dữ liệu từ mongodb
-app.get('/tasks', (req, res) => {
-    console.log('Get task data');
-  });
+    return res.redirect('/users');
 
-app.get('/tasks', async (req, res) => {
-    const tasks = await Task.find();
-    res.json(tasks);
-});
-// Route để lấy danh sách tất cả các tasks
-app.get('/tasks', (req, res) => {
-    res.json(tasks);
-  });
-  
-  // Route để tạo một task mới
-  app.post('/tasks', (req, res) => {
-    const newTask = req.body;
-    tasks.push(newTask);
-    res.json(newTask);
-  });
-  
-  // Route để lấy thông tin một task cụ thể
-  app.get('/tasks/:id', (req, res) => {
-    const id = req.params.id;
-    const task = tasks.find(task => task.id === id);
-    if (task) {
-      res.json(task);
-    } else {
-      res.status(404).json({ message: 'Task not found'});
-    }
-  });
-  
-  // Route để cập nhật thông tin một task
-  app.put('/tasks/:id', (req, res) => {
-    const id = req.params.id;
-    const updatedTask = req.body;
-    const index = tasks.findIndex(task => task.id === id);
-    if (index !== -1) {
-      tasks[index] = { ...tasks[index], ...updatedTask };
-      res.json(tasks[index]);
-    } else {
-      res.status(404).json({ message: 'Task not found' });
-    }
-  });
-  
-  // Route để xóa một task
-  app.delete('/tasks/:id', (req, res) => {
-    const id = req.params.id;
-    tasks = tasks.filter(task => task.id !== id);
-    res.json({ message: 'Task deleted' });
-  });
+
+
+})
+
   //Khsi báo thư mục chứa web
   app.use(express.static('public'));
   
@@ -127,7 +201,7 @@ app.get('/tasks', (req, res) => {
 
 
 
-  // Gửi yêu cầu để lấy danh sách công việc
+//   // Gửi yêu cầu để lấy danh sách công việc
 // fetch('/api/todos')
 // .then(response => response.json())
 // .then(todos => {
--- public/fontend.js
+++ public/fontend.js
@@ -1,83 +1,15 @@
-// const taskInput = document.getElementById('task');
-// const taskList = document.getElementById('taskList');
-// let tasks = []; // Mảng để lưu trữ danh sách công việc
+fetch('https://reqres.in/api/users',{
 
-// function addTask() {
-//     const taskText = taskInput.value;
-//     if (taskText !== '') {
-//         tasks.push({ text: taskText, completed: false });
-//         renderTasks();
-//         taskInput.value = '';
-//     }
-// }
-
-// function renderTasks() {
-//     taskList.innerHTML = '';
-//     tasks.forEach(task => {
-//         const li = document.createElement('li');
-//         li.textContent = task.text;   
-
-//         // Thêm các nút sửa, xóa, đánh dấu hoàn thành vào đây
-//         taskList.appendChild(li);
-//     });
-// }
-
-// function renderTasks() {
-//     taskList.innerHTML = '';
-//     tasks.forEach((task, index) => {
-//         const li = document.createElement('li');
-//         li.classList.add('task');
-//         li.innerHTML = `
-//             <span class="task-text">${task.text}</span>   
-
-//             <button class="delete" data-index="${index}">Xóa</button>
-//             <input type="checkbox" class="complete" ${task.completed ? 'checked' : ''}>
-//         `;
-//         taskList.appendChild(li);
-//     });
-
-//     // Xử lý sự kiện xóa
-//     const deleteButtons = document.querySelectorAll('.delete');
-//     deleteButtons.forEach(button => {
-//         button.addEventListener('click', () => {
-//             const index = button.dataset.index;   
-
-//             tasks.splice(index, 1);
-//             renderTasks();
-//         });
-//     });
-
-//     // Xử lý sự kiện đánh dấu hoàn thành
-//     const checkboxes = document.querySelectorAll('.complete');
-//     checkboxes.forEach(checkbox => {
-//         checkbox.addEventListener('change', () => {
-//             const index = checkbox.parentNode.querySelector('.delete').dataset.index;
-//             tasks[index].completed = checkbox.checked;
-//             renderTasks();
-//         });
-//     });
-// }
-
-function loadData(){
-    var myListContent ="";
-    //gọi API trả dữ liệu bằng hàng fetch
-    fetch('https://thao225.github.io/Demo_MNM/public/')
-    .then(response => response.json())
-    .then(todos => {
-    // Hiển thị danh sách công việc lên giao diện
-    })
-    .catch(error => console.error('Error:', error));
-    // Thêm một công việc mới
-    fetch('https://thao225.github.io/Demo_MNM/public/', {
-    method: 'POST',
-    headers: {
-    'Content-Type': 'application/json'
+    method:'POST',
+    headers:{
+        'content-type': 'application/json'
     },
-    body: JSON.stringify({ task: 'Learn Fetch API' })
-    })
-    .then(response => response.json())
-    .then(todo => console.log('Todo created:', todo))
-    .catch(error => console.error('Error:', error));
-}
+    body: JSON.stringify({
 
-loadData();
+        name:'user 6'
+    })
+}).then(res => {
+    return res.json()
+})
+.then(data => console.log(data))
+    .catch(error => console.log('ERROR'))
